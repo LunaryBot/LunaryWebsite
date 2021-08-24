0ea2a6ad8c01623bba17b362068f83d40ad8e076
JVOPINHO <jvopinho.dev@gmail.com>
2021-08-23 23:34:19 -0300

    Adicionando menu de guilds

--- src/routers/dashboard.js
+++ src/routers/dashboard.js
@@ -1,4 +1,5 @@
 const router = require("express").Router()
+const { Permissions } = require("discord.js")
 const checkAuth = require("../utils/checkAuth")
 
 router.get("/", checkAuth, (req, res) => {
@@ -7,7 +8,8 @@ router.get("/", checkAuth, (req, res) => {
 
 router.get("/@me", checkAuth, (req, res) => {
     res.render("dashboard/profile/main", {
-        user: req.user
+        user: req.user,
+        permissions: Permissions
     })
 })
 
